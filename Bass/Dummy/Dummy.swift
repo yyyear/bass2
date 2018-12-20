996556eb415619c5137ad54a157703c82a554125
Matt Cooper <matt.cooper@audiogum.com>
2018-12-20 15:22:52 +0000

    Initial commit

---
+++ Bass/Dummy/Dummy.swift
@@ -0,0 +1,13 @@
+//
+//  Dummy.swift
+//  Bass
+//
+//  Created by Alexander Orlov on 13/04/16.
+//  Copyright © 2016 Alexander Orlov. All rights reserved.
+//
+
+public struct ___dummy {
+    public func ___dummy() {
+        //
+    }
+}
